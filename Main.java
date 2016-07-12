233293ef73f34d5bd86faab334a16c796739be6e
juanzea94 <zea15401@uvg.edu.gt>
2016-07-12 12:53:31 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/** 
+*@author Juan Pablo Zea Rubio
+*@version 012.07.2016
+*/
+
+import java.util.Scanner;
+
+public class Principal{
+	public static void main(String[] args){
+		Scanner teclado = new Scanner (System.in);
+
+		System.out.println("Bienvenidos a la SUPER RADIO UVG")
+
+		System.out.println("Porfavor Utilice el radio con Cariño")
+		
+	}
+}
