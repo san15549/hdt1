47f07c2a9a898fe56ae39d64bafcd6ff493395dd
san15549 <san15549@uvg.edu.gt>
2016-07-12 17:14:39 -0600

    Update iRadio.java

--- iRadio.java
+++ iRadio.java
@@ -1,23 +1,25 @@
+/**
+ * @author Carlo David Sanchinelli, Juan Pablo Zea, Luis Najera
+ * @version 13.07.16
+ */
 public interface iRadio{ 
     void On();
-    
+	
     void Off();
        
     boolean isOn();
 
     String getFrequency();
-    
+	
     void setFrequency(String frequency);
-    
+	
     String getStation();
-    
+	
     void setStation(int position);
-    
+	
     void Forward();
-    
+	
     void Backward();
-    
+	
     void setMemory(int position);
-    
-    String toString();
 }
