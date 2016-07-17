32b1079a1e7d8dcedaa7e48b1c83ec891644f018
juanzea94 <zea15401@uvg.edu.gt>
2016-07-17 12:58:26 -0600

    Update iRadio.java

--- iRadio.java
+++ iRadio.java
@@ -1,24 +1,28 @@
 /**
+ * @place Universidad del Valle de Guatemala
+ * @class Algoritmos y Estructuras de Datos
  * @author Carlo David Sanchinelli, Juan Pablo Zea, Luis Najera
  * @version 13.07.16
  */
+/**Se crea la interfaz iRadio que nos permitira poder asociar cualquier otro trabajo de otro grupo con el nuestro*/
 public interface iRadio{ 
+    /**Se crean los parametros de salida y entrada de los métodos declarados en la clase Radio*/
     public void OnOff();
        
     public boolean isOn();
 
     public String getFrequency();
-	
+    
     public void setFrequency(String frequency);
-	
+    
     public String getStation();
-	
+    
     public void setStation(int position);
-	
+    
     public void Forward();
-	
+    
     public void Backward();
-	
+    
     public void setMemory(int position);
     
     public String toString();
